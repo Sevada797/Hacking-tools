b98a47125347a9e4905a16070461f2e1b04684f0
Sevada797 <64130935+Sevada797@users.noreply.github.com>
2020-05-26 18:11:14 +0400

    Update Hack-tools.py

--- Hack-tools.py
+++ Hack-tools.py
@@ -2,22 +2,23 @@
 #You can install a list of things from here and use
 import os
 import sys
+import subprocess
 os.system ("clear") 
+print ("\033[1;32;40m installing some usefull things")
+print ("\033[1;32;40m Installing PHP")
+time.sleep(2)
+subprocess.call('pkg install php', shell=True)
+print ("\033[1;32;40m Installing nano(for editing files).")
+time.sleep(3)
+subprocess.call('pkg install nano', shell=True)
+os.system ("clear")
 logo = """
  ____                      _      _____ ___ _____
 / ___|  _____   ____ _  __| | __ |___  / _ \___  |
 \___ \ / _ \ \ / / _` |/ _` |/ _` | / / (_) | / /
  ___) |  __/\ V / (_| | (_| | (_| |/ / \__, |/ /
-|____/ \___| \_/ \__,_|\__,_|\__,_/_/    /_//_/   """
+|____/ \___| \_/ \__,_|\__,_|\__,_/_/    /_//_/  \n """
 print ("\033[1;32;40m" + logo)
-print ("\033[1;32;40m installing some usefull things")
-print ("\033[1;32;40m Installing PHP")
-time.sleep(1)
-pkg install php
-print ("\033[1;32;40m Installing nano(for editing files).")
-time.sleep(3)
-pkg install nano
-os.system ("clear")
 print ("\033[1;31;40m Some hacking programs for you to install \n"
 print ("\033[1;32;40m 1)Phishing")
 print ("\033[1;32;40m 2)Fb brute-force")
