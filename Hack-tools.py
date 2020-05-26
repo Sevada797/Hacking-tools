dcaf0007b8c4aa244845cc574f9b758341d7a1bf
Sevada797 <64130935+Sevada797@users.noreply.github.com>
2020-05-26 18:02:05 +0400

    Update Hack-tools.py

--- Hack-tools.py
+++ Hack-tools.py
@@ -9,19 +9,20 @@ logo = """
 \___ \ / _ \ \ / / _` |/ _` |/ _` | / / (_) | / /
  ___) |  __/\ V / (_| | (_| | (_| |/ / \__, |/ /
 |____/ \___| \_/ \__,_|\__,_|\__,_/_/    /_//_/   """
-print "\033[1;32;40m" + logo
-print "\033[1;32;40m installing some usefull things"
-print "\033[1;32;40m Installing PHP"
+print ("\033[1;32;40m" + logo)
+print ("\033[1;32;40m installing some usefull things")
+print ("\033[1;32;40m Installing PHP")
 time.sleep(1)
 pkg install php
-print "\033[1;32;40m Installing nano(for editing files)."
+print ("\033[1;32;40m Installing nano(for editing files).")
 time.sleep(3)
 pkg install nano
 os.system ("clear")
-print "\033[1;31;40m Some hacking programs for you to install \n"
-print "\033[1;32;40m 1)Phishing"
-print "\033[1;32;40m 2)Fb brute-force"
-print "\033[1;32;40m 3)Insta brute-force"
-print "\033[1;32;40m 4)SMS bombing"
-print "\033[1;32;40m 5)Fakemailer \n"
-print "\033[1;31;40m Program is not working now. Building is in progress."
+print ("\033[1;31;40m Some hacking programs for you to install \n"
+print ("\033[1;32;40m 1)Phishing")
+print ("\033[1;32;40m 2)Fb brute-force")
+print ("\033[1;32;40m 3)Fb friends info gather")
+print ("\033[1;32;40m 4)Insta brute-force")
+print ("\033[1;32;40m 5)SMS bombing")
+print ("\033[1;32;40m 6)Fakemailer \n")
+print ("\033[1;31;40m Program is not working now. Building is in progress.")
