9584c35b12c62ea116a64d22cf133f10190f8380
Sevada797 <64130935+Sevada797@users.noreply.github.com>
2020-05-26 16:59:43 +0400

    Create Hack-tools.py

---
+++ Hack-tools.py
@@ -0,0 +1,25 @@
+#This is a comment
+#You can install a list of things from here and use
+import os
+os.system ("clear")
+print "\033[1;33;40m
+ ____                      _      _____ ___ _____
+/ ___|  _____   ____ _  __| | __ |___  / _ \___  |
+\___ \ / _ \ \ / / _` |/ _` |/ _` | / / (_) | / /
+ ___) |  __/\ V / (_| | (_| | (_| |/ / \__, |/ /
+|____/ \___| \_/ \__,_|\__,_|\__,_/_/    /_//_/   "
+print "\033[1;32;40m installing some usefull things"
+print "\033[1;32;40m Installing PHP"
+time.sleep(1)
+pkg install php
+print "\033[1;32;40m Installing nano(for editing files)."
+time.sleep(3)
+pkg install nano
+os.system ("clear")
+print "\033[1;31;40m Some hacking programs for you to install \n"
+print "\033[1;32;40m 1)Phishing"
+print "\033[1;32;40m 2)Fb brute-force"
+print "\033[1;32;40m 3)Insta brute-force"
+print "\033[1;32;40m 4)SMS bombing"
+print "\033[1;32;40m 5)Fakemailer \n"
+print "\033[1;31;40m Program is not working now. Building is in progress."
