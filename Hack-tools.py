0eda677443d5b81d0df1706d300b731a1d0f3c4f
Sevada797 <64130935+Sevada797@users.noreply.github.com>
2020-05-26 18:35:30 +0400

    Update Hack-tools.py

--- Hack-tools.py
+++ Hack-tools.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import subprocess
-os.system ("clear") 
+os.system('clear') 
 print ("\033[1;32;40m installing some usefull things")
 print ("\033[1;32;40m Installing PHP")
 time.sleep(2)
@@ -11,7 +11,7 @@ subprocess.call('pkg install php', shell=True)
 print ("\033[1;32;40m Installing nano(for editing files).")
 time.sleep(3)
 subprocess.call('pkg install nano', shell=True)
-os.system ("clear")
+os.system('clear')
 logo = """
  ____                      _      _____ ___ _____
 / ___|  _____   ____ _  __| | __ |___  / _ \___  |
@@ -19,7 +19,7 @@ logo = """
  ___) |  __/\ V / (_| | (_| | (_| |/ / \__, |/ /
 |____/ \___| \_/ \__,_|\__,_|\__,_/_/    /_//_/  \n """
 print ("\033[1;32;40m" + logo)
-print ("\033[1;31;40m Some hacking programs for you to install \n"
+print ("\033[1;31;40m Some hacking programs for you to install \n")
 print ("\033[1;32;40m Press 1 for Phishing")
 print ("\033[1;32;40m Press 2 for Fb brute-force")
 print ("\033[1;32;40m Press 3 for Fb friends info gather")
